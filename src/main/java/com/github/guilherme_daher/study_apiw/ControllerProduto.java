105afdf2f995deb183d13bfa033297ff2bdd395c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-10 21:01:01 -0300

    aula3

--- src/main/java/com/github/guilherme_daher/study_apiw/ControllerProduto.java
+++ src/main/java/com/github/guilherme_daher/study_apiw/ControllerProduto.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class ControllerProduto {
 
 
     @PostMapping
-    public ResponseEntity <String> create(){
+    public ResponseEntity <String> create(@RequestBody Produto produto){
         return ResponseEntity.status(201).body("Produto Cadastrado: Laranja!");
     }
         
